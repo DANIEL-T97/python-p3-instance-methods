bcce21c141ffb48ca98065406f050bf2309321e1
Daniel <daniel.mwangi@student.moringaschool.com>
2023-11-29 13:03:56 +0300

    committed insatance methods

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
 class Dog:
+    def sit(self):
+        print("The dog is sitting.")
+        pass
+    def bark(self):
+        print("Woof!")
     # Class body goes here
 
     #Instance method definition
-    pass
+fido = Dog()
+fido.bark()
+    
+snoopy = Dog()
+snoopy.sit()
